e6a2c987c65ad86110070d3cb3be8164f2d86b4c
Mark <mark@remarkablemark.org>
2023-06-14 21:38:20 -0400

    test: colocate tests and improve test description

--- module/index.test.mjs
+++ module/index.test.mjs
@@ -9,28 +9,28 @@ describe('index', () => {
     assert.strictEqual(typeof digest, 'function');
   });
 
-  it('returns "SHA-1"', async () => {
+  it('hashes message with algorithm "SHA-1"', async () => {
     assert.strictEqual(
       await digest('SHA-1', ''),
       'da39a3ee5e6b4b0d3255bfef95601890afd80709'
     );
   });
 
-  it('returns "SHA-256"', async () => {
+  it('hashes message with algorithm "SHA-256"', async () => {
     assert.strictEqual(
       await digest('SHA-256', ''),
       'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
     );
   });
 
-  it('returns "SHA-384"', async () => {
+  it('hashes message with algorithm "SHA-384"', async () => {
     assert.strictEqual(
       await digest('SHA-384', ''),
       '38b060a751ac96384cd9327eb1b1e36a21fdb71114be07434c0cc7bf63f6e1da274edebfe76f65fbd51ad2f14898b95b'
     );
   });
 
-  it('returns "SHA-512"', async () => {
+  it('hashes message with algorithm "SHA-512"', async () => {
     assert.strictEqual(
       await digest('SHA-384', ''),
       '38b060a751ac96384cd9327eb1b1e36a21fdb71114be07434c0cc7bf63f6e1da274edebfe76f65fbd51ad2f14898b95b'
--- src/index.test.ts
+++ src/index.test.ts
@@ -1,10 +1,8 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import { digest } from '../src/index.ts';
+import { digest } from './index';
 
 describe('digest', () => {
   it.each(['SHA-1', 'SHA-256', 'SHA-384', 'SHA-512'] as const)(
-    'hashes message with algorithm %s',
+    'hashes message with algorithm "%s"',
     async (algorithm) => {
       expect(await digest(algorithm, '')).toMatchSnapshot();
     }
